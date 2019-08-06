a29f3fc2786e2d25666d7966041f0dfe2778e655
masonkarsevar <masonkarsevar@gmail.com>
2019-08-06 12:37:36 -0700

    UserCard styling finished. WIP: input more info into cards

--- github-project-react/src/components/UserList.js
+++ github-project-react/src/components/UserList.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import UserCard from './UserCard';
+
+const UserListDiv = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+`;
 
 const UserList = props => {
     // console.log('from UserList', props);
     return (
-        <div>
+        <UserListDiv>
             {props.userData.map((user, index) => {
                 return (
                     <UserCard user={user} key={index} />
                 )
             })}
-        </div>
+        </UserListDiv>
     )
 }
 
