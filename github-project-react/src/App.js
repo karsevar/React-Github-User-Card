ce0e01ab66341982231eb1dbc8b0b5e658554a02
masonkarsevar <masonkarsevar@gmail.com>
2019-08-06 08:59:37 -0700

    data is coming back from the server for all of the followers

--- github-project-react/src/App.js
+++ github-project-react/src/App.js
@@ -8,7 +8,26 @@ class App extends Component {
   componentDidMount() {
     axios
       .get('https://api.github.com/users/tetondan')
-      .then(results => console.log(results))
+      .then(results => { 
+        console.log('main user info', results.data)
+
+        axios.get(results.data.followers_url)
+          .then(results => {
+            console.log('followers data', results.data)
+            
+            const followersAuto = results.data.map(follower => follower.login)
+
+            followersAuto.forEach(follower => {
+              axios.get(`https://api.github.com/users/${follower}`)
+                .then(result => {
+                  console.log('data from followers', result.data)
+                })
+                .catch(error => {
+                  console.log('server error', error);
+                })
+            })
+          })
+      })
       .catch(err => console.log('did not work', err))
   }
   
