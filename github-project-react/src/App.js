fa68250a608247b99f55eed13b7a47c2db746def
masonkarsevar <masonkarsevar@gmail.com>
2019-08-06 12:10:47 -0700

    now state has all of the information for the followers

--- github-project-react/src/App.js
+++ github-project-react/src/App.js
@@ -16,18 +16,24 @@ class App extends Component {
       .get('https://api.github.com/users/tetondan')
       .then(results => { 
         // console.log('main user info', results.data)
+
+        // First gets the data from the main account of the api call
         this.setState({userData: [results.data]})
 
         axios.get(results.data.followers_url)
           .then(results => {
             // console.log('followers data', results.data)
             
+            // Finds the usernames of all of the followers.
             const followersAuto = results.data.map(follower => follower.login)
 
             followersAuto.forEach(follower => {
               axios.get(`https://api.github.com/users/${follower}`)
                 .then(result => {
                   // console.log('data from followers', result.data)
+
+                  // Inputs all of the information of the followers into this.state.
+                  this.setState({userData: [...this.state.userData, result.data]})
                 })
                 .catch(error => {
                   console.log('server error', error);
