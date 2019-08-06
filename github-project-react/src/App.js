00e116c8df07d29e2e32a2b2d93f653eb35e0ba1
masonkarsevar <masonkarsevar@gmail.com>
2019-08-06 12:06:01 -0700

    new data can be inputted into this.state

--- github-project-react/src/App.js
+++ github-project-react/src/App.js
@@ -4,23 +4,30 @@ import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userData: []
+    }
+  }
   
   componentDidMount() {
     axios
       .get('https://api.github.com/users/tetondan')
       .then(results => { 
-        console.log('main user info', results.data)
+        // console.log('main user info', results.data)
+        this.setState({userData: [results.data]})
 
         axios.get(results.data.followers_url)
           .then(results => {
-            console.log('followers data', results.data)
+            // console.log('followers data', results.data)
             
             const followersAuto = results.data.map(follower => follower.login)
 
             followersAuto.forEach(follower => {
               axios.get(`https://api.github.com/users/${follower}`)
                 .then(result => {
-                  console.log('data from followers', result.data)
+                  // console.log('data from followers', result.data)
                 })
                 .catch(error => {
                   console.log('server error', error);
@@ -32,6 +39,7 @@ class App extends Component {
   }
   
   render() {
+    console.log('In render', this.state);
     return (
       <div className="App">
         <p>Component Mounted</p>
