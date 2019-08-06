98097d231ae3d95b4a2aa68f8270f82176d2537a
masonkarsevar <masonkarsevar@gmail.com>
2019-08-06 08:46:49 -0700

    basic app component class created

--- github-project-react/src/App.js
+++ github-project-react/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  componentDidMount() {
+    axios
+      .get('https://api.github.com/users/tetondan')
+      .then(results => console.log(results))
+      .catch(err => console.log('did not work', err))
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <p>Component Mounted</p>
+      </div>
+    );
+  }
 }
 
 export default App;
